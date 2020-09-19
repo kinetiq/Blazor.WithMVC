bba2a4f324d0c6153914072de3d43eea22b52d0f
Brian MacKay <brianm@getkinetiq.com>
2020-09-19 10:53:03 -0400

    Step 1: Blazor is working. I created a new Blazor App (using Blazor Server) in Visual Studio and folded it into this app, with some changes to make this as easy to understand as possible.
    Deviations from the Blazor App template include:
     - In _Host, referenced the existing bootstrap that the MVC app already had.
     - Consequently there was no need to bring bootstrap over.
     - Simplified the components. I made the Index page a counter control since that's a useful way to see that Blazor is working. Deleted everything else.
     - MVC already had a site.css, so referenced that and added a few classes for the gold error box that appears when there are Blazor errors.
     - In MainLayout, removed everything non-essential and added a container tag for padding.
    
    This is not beautifully styled, but it does show Blazor and MVC working together in one project.
    
    The problem here is that our MVC app expects the default route (http://localhost:[port]/) to go to home, but Blazor has taken that over. In the next commit I show a workaround.

--- Dev/Demo.Web/Startup.cs
+++ Dev/Demo.Web/Startup.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,13 +19,17 @@ namespace Demo.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // Add Razor Pages, which is the typical way to use Blazor.
+            services.AddRazorPages();
+
+            // Add Blazor. Note that no extra packages are needed. This is available as part of Core as of 3.0
+            services.AddServerSideBlazor();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +39,6 @@ namespace Demo.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -51,6 +53,15 @@ namespace Demo.Web
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                // Adds the hub, which is how Blazor components connect to the server via SignalR. 
+                endpoints.MapBlazorHub(); 
+
+                // Creates a low-priority route to the _Host file. In effect, all traffic that isn't explicitly routed
+                // elsewhere will go to _Host, where Blazor's internal routing will take over. This means your existing
+                // MVC routes will continue to work, *except* the default Route, which is a problem and we will work
+                // around that in the next commit.
+                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
